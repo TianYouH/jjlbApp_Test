f2195a33c80830276bd58d7a5ebfe84bb80e2093
huangjinliang <1406957473@qq.com>
2018-05-02 15:50:48 +0800

    临时

---
+++ app/pages/Test4.js
@@ -0,0 +1,29 @@
+
+import React, {Component} from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    Image,
+} from 'react-native';
+
+export default class Test4 extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>点我444</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+});
